34b9cf4282772ad6c2a2558be4b9b9c93454d6a7
DendyGrobovshik <idenis.gradoboev@yandex.com>
2022-06-12 21:12:56 +0500

    Change render function to component class

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,18 +2,33 @@ import { AppShell, Stack } from '@mantine/core';
 import Header from '~/components/Header';
 import Form from '~/components/Form';
 import Covers from '~/components/covers/Covers';
+import React from 'react';
 
-export default function Index() {
-  return (
-    <AppShell
-      navbar={<Stack><Header /><Form /></Stack>}
-      styles={{
-        main: {
-          padding: 0,
-        },
-      }}
-    >
-      <Covers />
-    </AppShell>
-  );
+export default class Index extends React.Component {
+  state = {
+    loading: true
+  };
+
+  componentDidMount() {
+    this.setState({ loading: false });
+  }
+
+  render() {
+    if (this.state.loading) {
+      return null;
+    }
+
+    return (
+      <AppShell
+        navbar={<Stack><Header /><Form /></Stack>}
+        styles={{
+          main: {
+            padding: 0,
+          },
+        }}
+      >
+        <Covers />
+      </AppShell>
+    );
+  }
 }
