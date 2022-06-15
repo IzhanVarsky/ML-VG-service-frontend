9b320b076fd351fe68ab6aff3790917120f0c65e
IzhanVarsky <48652648+IzhanVarsky@users.noreply.github.com>
2022-06-15 11:11:37 +0300

    regex bug fixed

--- app/utils.js
+++ app/utils.js
@@ -1,4 +1,5 @@
-const correct_color_regex = "^(#([\\da-f]{3}){1,2}|(rgb|hsl)a\\((\\d{1,3}%?,\\s?){3}(1|0?\\.\\d+)\\)|(rgb|hsl)\\(\\d{1,3}%?(,\\s?\\d{1,3}%?){2}\\))$";
+const correct_color_regex =
+  "^(#([\\da-f]{3}){1,2}|(rgb|hsl)a\\((\\d{1,3}%?,\\s?){3}(1|1\\.0+|0|0?\\.\\d+)\\)|(rgb|hsl)\\(\\d{1,3}%?(,\\s?\\d{1,3}%?){2}\\))$";
 const re = new RegExp(correct_color_regex);
 
 function findColorByAttrName(obj, attr) {
